3fe9cbf0c681c03fd69b6e10449cfa921f274156
theubermanishere <vinayakkaushikk@gmail.com>
2017-02-02 22:49:12 +0530

    Some code cleanup still left.

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 # Multi-threaded python program to download xkcd comics, along with the heading and the alt-text.
 __author__ = 'TheUbermanIsHere'
 
-# The Program must check for all of the xkcd comics that have been downloaded uptill now.
-# All the comics that are missing will be downloaded.
-# The downloaded files must be saved as png's with their numbers as the filename.
-# The comics title must be added to the top and the alt-text will be added to the bottom.
-
 import re
 import os
 import requests
@@ -13,11 +8,8 @@ import bs4
 from PIL import Image,ImageDraw,ImageFont
 import textwrap
 
-# Create a list of all the files that are present in the current directory.
-
 pngReg = re.compile(r'[a-zA-Z0-9]*.(png|jpg)')
 dirdump = os.listdir()
-
 
 for i in reversed(dirdump):
     b = pngReg.search(i)
@@ -34,17 +26,9 @@ for i in dirdump:
 
 print(dirlist)
 
-# Go to the homepage and check the number of the latest comic, and initialize a counter with the number.
-
 home = requests.get('http://xkcd.com')
 home.raise_for_status()
 bs = bs4.BeautifulSoup(home.text,"lxml")
-title = bs.select('#ctitle')
-print(title[0].getText())
-
-alttext = bs.select('img')
-alttext = alttext[1].attrs
-print(alttext['title'])
 
 text = bs.select('#middleContainer')
 textRe = re.compile(r'http.*\.png')
@@ -62,9 +46,6 @@ counter = int(bb.group())
 
 downlist = list(i for i in range(1,counter))
 downlist = list(set(downlist) - set(dirlist))
-
-# The Download list has been initialized.
-# Function that download the comics from the given list.
 
 def xkcd(a):
     for i in a:
@@ -93,17 +74,14 @@ def xkcd(a):
 
         initRe = re.compile(r'http://xk.*/')
         b = initRe.search(text[0].getText())
-        print(b.group())
 
         bRe = re.compile(r'\d\d\d\d|\d\d\d|\d\d|\d')
         bb = bRe.search(b.group())
-        print(bb.group())
 
         count = int(bb.group())
         downImage(aa.group(),count,imgform,title,alt)
 
 
-# Main-function that handles everything from downloading, to image processing, to saving the file.
 def downImage(a,count,imgform,title,alt):
     image = requests.get(a)
     image.raise_for_status()
@@ -118,12 +96,12 @@ def downImage(a,count,imgform,title,alt):
     ximage.close()
     ximage = Image.open(filename)
     width, height = ximage.size
-    arvo = ImageFont.truetype('xkcd.otf',22)
+    xkcdFont = ImageFont.truetype('xkcd.otf',22)
     im = Image.new('RGBA', (width,70+height+160), 'black')
     im.paste(ximage, (0, 70))
     draw = ImageDraw.Draw(im)
     w,h = draw.textsize(title)
-    draw.text(((width-w)/2-(w/2),35),title, fill='white', font=arvo)
+    draw.text(((width-w)/2-(w/2),35),title, fill='white', font=xkcdFont)
     cc = 20 
     widthh = 50
     if width<510:
@@ -137,11 +115,4 @@ def downImage(a,count,imgform,title,alt):
     im.save(filename)
 
 xkcd(downlist)
-
-# To-do:
-# 1. Get the size of the image.
-# 2. Figure out the size of the text that must be pasted
-# 3. Open a new image, and paste the downloaded image on top of that
-# 4. Put the heading and the alt-text in place.
-# 5. Save the Image as a png file.
 
