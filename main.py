1c51e13cee77409c6c0189c5b588013deac999f9
Jason L Causey <jason@jasoncausey.net>
2024-12-19 09:17:16 -0600

    updated to fix font and especially alt-text centering and sizing issues

--- main.py
+++ main.py
@@ -1,125 +1,132 @@
+#!/usr/bin/env python3
+"""
+Adapted from https://github.com/theubermanishere/xkcd-comic-downloader
+by 'TheUbermanIsHere'
+
+This script will take a comic number (or list of comic numbers) on the command-line 
+and download the images for those comics.  It adds the alt-text and heading to the 
+image.
+"""
 # Multi-threaded python program to download xkcd comics, along with the heading and the alt-text.
-__author__ = 'TheUbermanIsHere'
+__author__ = ["TheUbermanIsHere", "jasoncausey"]
 
 import re
 import os
 import requests
 import bs4
-from PIL import Image,ImageDraw,ImageFont
+import argparse
+import unicodedata
+from PIL import Image, ImageDraw, ImageFont
 import textwrap
 
-pngReg = re.compile(r'[a-zA-Z0-9]*.(png|jpg)')
-dirdump = os.listdir()
 
-for i in reversed(dirdump):
-    b = pngReg.search(i)
-    if b is None:
-        dirdump.remove(i)
+def slugify(value, allow_unicode=False):
+    """
+    Taken from https://github.com/django/django/blob/master/django/utils/text.py
+    Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
+    dashes to underscores. Remove characters that aren't alphanumerics,
+    underscores, or hyphens. Convert to lowercase. Also strip leading and
+    trailing whitespace, dashes, and underscores.
+    """
+    value = str(value)
+    if allow_unicode:
+        value = unicodedata.normalize("NFKC", value)
+    else:
+        value = (
+            unicodedata.normalize("NFKD", value)
+            .encode("ascii", "ignore")
+            .decode("ascii")
+        )
+    value = re.sub(r"[^\w\s-]", "", value.lower())
+    return re.sub(r"[-\s]+", "_", value).strip("-_")
 
-dirdump = sorted(dirdump)
-dirlist = list()
-
-for i in dirdump:
-    i = i.split('.')
-    i = i[0]
-    dirlist.append(int(i))
-dirlist.append(404)
-
-print(dirlist)
-
-home = requests.get('http://xkcd.com')
-home.raise_for_status()
-bs = bs4.BeautifulSoup(home.text,"lxml")
-
-text = bs.select('#middleContainer')
-textRe = re.compile(r'http.*\.png')
-aa = textRe.search(text[0].getText())
-
-initRe = re.compile(r'http://xk.*/')
-b = initRe.search(text[0].getText())
-
-bRe = re.compile(r'\d\d\d\d')
-bb = bRe.search(b.group())
-
-counter = int(bb.group())
-
-# Create a list of all the comics to be downloaded.
-
-downlist = list(i for i in range(1,counter))
-downlist = list(set(downlist) - set(dirlist))
 
 def xkcd(a):
     for i in a:
         print("Currently downloading comic: " + str(i))
-        page = requests.get('http://xkcd.com/' + str(i))
+        page = requests.get("http://xkcd.com/" + str(i))
         page.raise_for_status()
-        bs = bs4.BeautifulSoup(page.text,"lxml")
-        title = bs.select('#ctitle')
+        bs = bs4.BeautifulSoup(page.text, "lxml")
+        title = bs.select("#ctitle")
         title = title[0].getText()
         print(title)
 
-        alttext = bs.select('img')
-        alttext = alttext[1].attrs
-        alt = alttext['title']
+        alttext = bs.select("img")
+        i = 1
+        while "title" not in alttext[i].attrs:
+            i += 1
+        alttext = alttext[i].attrs
+
+        alt = alttext["title"]
         print(alt)
 
-        text = bs.select('#middleContainer')
-        textRe = re.compile(r'http.*\.png')
+        text = bs.select("#middleContainer")
+        textRe = re.compile(r"http.*\.png")
         aa = textRe.search(text[0].getText())
-        imgform = 'png'
+        imgform = "png"
         if aa is None:
-            textRe = re.compile(r'http.*\.jpg')
+            textRe = re.compile(r"http.*\.jpg")
             aa = textRe.search(text[0].getText())
-            imgform = 'jpg'
+            imgform = "jpg"
 
-
-        initRe = re.compile(r'http://xk.*/')
+        initRe = re.compile(r"https://xk.*/")
         b = initRe.search(text[0].getText())
 
-        bRe = re.compile(r'\d\d\d\d|\d\d\d|\d\d|\d')
+        bRe = re.compile(r"\d\d\d\d|\d\d\d|\d\d|\d")
         bb = bRe.search(b.group())
 
         count = int(bb.group())
-        downImage(aa.group(),count,imgform,title,alt)
+        downImage(aa.group(), count, imgform, title, alt)
 
 
-def downImage(a,count,imgform,title,alt):
+def downImage(a, count, imgform, title, alt):
     image = requests.get(a)
     image.raise_for_status()
-    if imgform == 'png':
-        filename = str(count)+'.png'
-        ximage = open(filename,'wb')
+    file_base = slugify(title)
+    if imgform == "png":
+        filename = file_base + ".png"
+        ximage = open(filename, "wb")
     else:
-        filename = str(count)+'.jpg'
-        ximage = open(filename,'wb')
+        filename = file_base + ".jpg"
+        ximage = open(filename, "wb")
     for i in image.iter_content(100000):
         ximage.write(i)
     ximage.close()
     ximage = Image.open(filename)
     width, height = ximage.size
-    xkcdFont = ImageFont.truetype('xkcd.otf',22)
-    im = Image.new('RGBA', (width,70+height+200), 'black')
+    font_size = 22
+    xkcdFont = ImageFont.truetype("xkcd.otf", font_size)
+    im = Image.new("RGBA", (width, 70 + height + 200), "black")
     im.paste(ximage, (0, 70))
     draw = ImageDraw.Draw(im)
-    w,h = draw.textsize(title)
-    draw.text(((width-w)/2-(w/2),35),title, fill='white', font=xkcdFont)
-    cc = 20 
+    w = draw.textlength(title, font=xkcdFont)
+    draw.text(((width - w) / 2, font_size + 10), title, fill="white", font=xkcdFont)
     widthh = 50
-    if width<551:
+    if width < 551:
         widthh = 40
-    if width<461:
+    if width < 461:
         widthh = 35
-    if width<410:
+    if width < 410:
         widthh = 30
-    if width<360:
+    if width < 360:
         widthh = 25
-    if width<310:
+    if width < 310:
         widthh = 20
-    for line in textwrap.wrap(alt, width=widthh):
-        w,h = draw.textsize(line)
-        draw.text(((width-w)/2-(w/2),70+height+cc),line, fill='white', font=xkcdFont)
-        cc += 20
+    for idx, line in enumerate(textwrap.wrap(alt, width=widthh)):
+        line = line.strip()
+        w = draw.textlength(line, font=xkcdFont)
+        draw.text(
+            ((width - w) / 2 , 70 + height + font_size * (idx+1)),
+            line,
+            fill="white",
+            font=xkcdFont,
+        )
     im.save(filename)
 
-xkcd(downlist)
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("comic_number", metavar="comic-number", type=str, nargs="+")
+    args = parser.parse_args()
+    comics = args.comic_number
+    xkcd(comics)
